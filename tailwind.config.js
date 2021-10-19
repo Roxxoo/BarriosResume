f899066246d28dfa7fe905a5ebd65425d43f7994
Jeffrey <rxbarrios18@gmail.com>
2021-10-19 09:32:04 +0800

    Added max-width and fixed the error regarding build run

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,26 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    screens:{
-      '4k': '2199px;'
-    },
-    extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+      '2k' : '2048px',
+
+      '4k' : '3840px',
+    }
+    
   },
   variants: {
     extend: {},
